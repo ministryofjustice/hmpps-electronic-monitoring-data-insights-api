aec6bf0827f1f8c201feb6eb9dfbc880e8238b77
Johny Farrar <me@johnyfarrar.co.uk>
2026-01-12 10:27:04 +0000

    Update DeviceEvents.kt

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatainsightsapi/deviceevents/DeviceEvents.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatainsightsapi/deviceevents/DeviceEvents.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.json.Json
 import org.jetbrains.exposed.sql.Table
 import org.jetbrains.exposed.sql.javatime.datetime
 import org.jetbrains.exposed.sql.json.jsonb
-import uk.gov.justice.digital.hmpps.electronicmonitoringdatainsightsapi.model.entity.Events
+import uk.gov.justice.digital.hmpps.electronicmonitoringdatainsightsapi.device.model.Events
 import java.util.UUID
 
 val json = Json { prettyPrint = true }
