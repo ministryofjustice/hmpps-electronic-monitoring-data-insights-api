7a8edd9dbec34cc91fc4903a4c257e9add07dcb9
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2026-03-18 10:30:22 +0000

    changes to accept these dependency updates.

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatainsightsapi/ElectronicMonitoringDataInsightsApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatainsightsapi/ElectronicMonitoringDataInsightsApi.kt
@@ -1,7 +1,7 @@
 
 package uk.gov.justice.digital.hmpps.electronicmonitoringdatainsightsapi
-import org.jetbrains.exposed.sql.SchemaUtils
-import org.jetbrains.exposed.sql.transactions.transaction
+import org.jetbrains.exposed.v1.jdbc.SchemaUtils
+import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatainsightsapi.device.model.DeviceEvents
