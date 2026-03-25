8fdf925a567709a60ebc8aca349ea001d6545a0b
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2026-03-25 08:52:49 +0000

    temp branch to get app running locally for swagger

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatainsightsapi/ElectronicMonitoringDataInsightsApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatainsightsapi/ElectronicMonitoringDataInsightsApi.kt
@@ -1,12 +1,7 @@
 
 package uk.gov.justice.digital.hmpps.electronicmonitoringdatainsightsapi
-import org.jetbrains.exposed.v1.jdbc.SchemaUtils
-import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import uk.gov.justice.digital.hmpps.electronicmonitoringdatainsightsapi.device.model.DeviceEvents
-import uk.gov.justice.digital.hmpps.electronicmonitoringdatainsightsapi.location.model.Locations
-import uk.gov.justice.digital.hmpps.electronicmonitoringdatainsightsapi.watermark.Watermarks
 
 @SpringBootApplication
 class ElectronicMonitoringDataInsightsApi
