24ea834f4974594343a23ffaed7fe62903d62c85
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2026-03-27 14:40:58 +0000

    Update to java 25

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
   val kotlinVersion = "2.3.20"
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.1.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "10.0.5"
   kotlin("plugin.spring") version kotlinVersion
   kotlin("plugin.serialization") version kotlinVersion
 }
---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatainsightsapi/config/DevSecurityConfig.kt
@@ -0,0 +1,29 @@
+package uk.gov.justice.digital.hmpps.electronicmonitoringdatainsightsapi.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Profile("dev")
+@Configuration
+class DevSecurityConfig {
+
+  @Bean
+  fun filterChain(http: HttpSecurity): SecurityFilterChain {
+    http
+      .authorizeHttpRequests {
+        it.requestMatchers("/sync/**").permitAll()
+        it.requestMatchers("/people/**").permitAll()
+        it.requestMatchers("/v3/api-docs/**").permitAll()
+        it.requestMatchers("/swagger-ui/**").permitAll()
+        it.requestMatchers("/swagger-ui.html").permitAll()
+        it.requestMatchers("/v3/api-docs.yaml").permitAll()
+        it.requestMatchers("/health/ping").permitAll()
+        it.anyRequest().authenticated()
+      }
+      .csrf { it.disable() } // disable CSRF for local dev
+    return http.build()
+  }
+}
