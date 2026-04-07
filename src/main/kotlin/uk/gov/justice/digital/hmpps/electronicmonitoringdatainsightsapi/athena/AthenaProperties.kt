65c954585a73ec0a5b43fe81ad979d84a5766c6b
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2026-04-07 15:26:38 +0100

    increase number of records returned

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatainsightsapi/athena/AthenaProperties.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatainsightsapi/athena/AthenaProperties.kt
@@ -6,7 +6,7 @@ data class AthenaProperties(
   val fmsDatabase: String = "",
   val outputLocation: String = "",
   val workgroup: String? = null,
-  val rowLimit: Int = 100,
+  val rowLimit: Int = 20160,
   val pollIntervalMs: Long = 500,
   val timeoutMs: Long = 60000,
   val role: String? = null,
