c0a288c11e94564f7c64fa1ba380dbf78342e416
stevomcallister <78762879+stevomcallister@users.noreply.github.com>
2026-08-04 14:01:26 +0100

    removing old RDS code (#240)

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatainsightsapi/ElectronicMonitoringDataInsightsApi.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatainsightsapi/ElectronicMonitoringDataInsightsApi.kt
@@ -1,13 +1,8 @@
 
 package uk.gov.justice.digital.hmpps.electronicmonitoringdatainsightsapi
-import org.jetbrains.exposed.v1.jdbc.SchemaUtils
-import org.jetbrains.exposed.v1.jdbc.transactions.transaction
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
-import uk.gov.justice.digital.hmpps.electronicmonitoringdatainsightsapi.device.model.DeviceEvents
-import uk.gov.justice.digital.hmpps.electronicmonitoringdatainsightsapi.location.model.Locations
-import uk.gov.justice.digital.hmpps.electronicmonitoringdatainsightsapi.watermark.Watermarks
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
@@ -15,9 +10,4 @@ class ElectronicMonitoringDataInsightsApi
 
 fun main(args: Array<String>) {
   SpringApplication.run(ElectronicMonitoringDataInsightsApi::class.java, *args)
-  transaction {
-    SchemaUtils.create(DeviceEvents)
-    SchemaUtils.create(Locations)
-    SchemaUtils.create(Watermarks)
-  }
 }
