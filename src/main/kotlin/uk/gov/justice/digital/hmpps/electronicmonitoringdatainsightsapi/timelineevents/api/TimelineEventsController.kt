1de71d97531e09b1517334071624f041798dd191
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2026-08-11 13:31:27 +0100

    Adding timeline audit statistics

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatainsightsapi/timelineevents/api/TimelineEventsController.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatainsightsapi/timelineevents/api/TimelineEventsController.kt
@@ -4,19 +4,16 @@ import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.MediaType
-import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
-import uk.gov.justice.digital.hmpps.electronicmonitoringdatainsightsapi.common.HAS_VIEW_ROLE
 import uk.gov.justice.digital.hmpps.electronicmonitoringdatainsightsapi.timelineevents.service.TimelineEventsService
 import java.time.LocalDate
 import java.time.ZoneId
 
 @RestController
 @RequestMapping("/timeline-events", produces = [MediaType.TEXT_PLAIN_VALUE])
-@PreAuthorize(HAS_VIEW_ROLE)
 @Tag(name = "Timeline Events", description = "Endpoints for timeline event statistics")
 class TimelineEventsController(
   private val timelineEventsService: TimelineEventsService,
