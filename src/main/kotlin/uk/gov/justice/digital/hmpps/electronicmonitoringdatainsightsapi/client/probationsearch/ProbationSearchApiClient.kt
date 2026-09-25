4b3443869975e1272142ba5de42ed96a7f3608ea
stevomcallister <Stephen.Mcallister1@justice.gov.uk>
2026-09-25 15:28:28 +0100

    Added Best effort get probation areas

--- src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatainsightsapi/client/probationsearch/ProbationSearchApiClient.kt
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/electronicmonitoringdatainsightsapi/client/probationsearch/ProbationSearchApiClient.kt
@@ -1,11 +1,14 @@
 package uk.gov.justice.digital.hmpps.electronicmonitoringdatainsightsapi.client.probationsearch
 
+import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.bodyToFlux
 import reactor.core.publisher.Mono
+
+private val log = KotlinLogging.logger {}
 
 @Component
 class ProbationSearchApiClient(
@@ -33,10 +36,15 @@ class ProbationSearchApiClient(
 
   fun getProbationAreas(crn: String?): List<String> {
     if (crn == null) return emptyList()
-    return getOffendersByCrn(crn)
-      .filter { it.otherIds?.crn == crn }
-      .flatMap { it.offenderManagers }
-      .filter { it.active && !it.softDeleted }
-      .map { it.probationArea?.description ?: "Not Found" }
+    try {
+      return getOffendersByCrn(crn)
+        .filter { it.otherIds?.crn == crn }
+        .flatMap { it.offenderManagers }
+        .filter { it.active && !it.softDeleted }
+        .map { it.probationArea?.description ?: "Not Found" }
+    } catch (e: Exception) {
+      log.error("Error getting probation areas for crn=$crn", e)
+      return emptyList()
+    }
   }
 }
